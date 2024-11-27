8fb3bc66992e239d8c9d681b7f9d8abe482c0026
Zffu <radi0o0@outlook.com>
2024-11-27 23:31:58 +0100

    feat: added faststrmp function to header

---
+++ src/main.h
@@ -0,0 +1,8 @@
+/**
+ * The main file of the lib.
+ */
+
+/**
+ * Compares the two string given. If they are both the same, return 0 if not return another number.
+ */
+int faststrcmp(string* string1, string* string2);
