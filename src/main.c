73ced4ddad49ab22b28c841d1153a1f3bf591115
Zffu <radi0o0@outlook.com>
2024-11-28 00:36:34 +0100

    feat: added strhash func

--- src/main.c
+++ src/main.c
@@ -3,8 +3,15 @@
  */
 
 /**
- * Compares the two string given. If they are both the same, return 0 if not return another number.
+ * Compares the two string given. If they are both the same, return 1 if not return another number.
  */
 int faststrcmp(char* string1, char* string2) {
-	return ((long*)string1) - ((long*)string2);
+	return ((*(long*)string1) == (*(long*)string2)) ? 1 : 0;
+}
+
+/**
+ * Gets the "hash" of the string.
+ */
+long strhash(char* string) {
+	return (*(long*)string1);
 }
